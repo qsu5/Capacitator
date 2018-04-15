5caca7f8162794ea31a8c56cd6229446f9af41a1
Qingyang Su <Ken@Qingyangs-MacBook-Pro.local>
2018-04-15 11:15:28 -0500

    add graph file

---
+++ Basic/Graph.swift
@@ -0,0 +1,35 @@
+//
+//  Graph.swift
+//  Basic
+//
+//  Created by Qingyang Su on 4/14/18.
+//  Copyright © 2018 Qingyang Su. All rights reserved.
+//
+
+import UIKit
+
+class Graph: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
